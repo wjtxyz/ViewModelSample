460ee9246af738c3893687c733628bb8f9e5fbb5
WangJiangTao <wangjiangtao@ainirobot.com>
2021-02-08 12:06:40 +0800

    test the recyclerview items state saving

--- app/src/main/java/com/xxx/viewmodelsample/MainActivity.java
+++ app/src/main/java/com/xxx/viewmodelsample/MainActivity.java
@@ -3,11 +3,13 @@ package com.xxx.viewmodelsample;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.ObservableArrayList;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.xxx.viewmodelsample.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         binding.setViewModel(new ViewModelProvider(this).get(MainActivityViewModel.class));
+        binding.setFirstList(new ObservableArrayList<>());
         binding.setLifecycleOwner(this);
         setContentView(binding.getRoot());
     }
--- app/src/main/java/com/xxx/viewmodelsample/MainActivityViewModel.java
+++ app/src/main/java/com/xxx/viewmodelsample/MainActivityViewModel.java
@@ -3,18 +3,48 @@ package com.xxx.viewmodelsample;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableArrayList;
+import androidx.databinding.ObservableList;
 import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.SavedStateHandle;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivityViewModel extends AndroidViewModel {
+    private static final String TAG = "MainActivityViewModel";
     public final SavedStateHandle savedStateHandle;
 
     public final MutableLiveData<String> inputTextLiveData = new MutableLiveData<>();
 
+    public final List<String> secondList = new ObservableArrayList<>();
+
     public MainActivityViewModel(@NonNull Application application, SavedStateHandle savedStateHandle) {
         super(application);
         this.savedStateHandle = savedStateHandle;
+    }
+
+    public void onAddItem(List<String>... lists) {
+        final String sDate = String.valueOf(new Date());
+        Arrays.stream(lists).forEach(s -> s.add(sDate));
+    }
+
+    @SuppressWarnings("unchecked")
+    @NonNull
+    public MutableLiveData getLiveDataObservableListSavedState(@NonNull String name) {
+        final MutableLiveData liveData = savedStateHandle.getLiveData(name);
+        if (!(liveData.getValue() instanceof ObservableList)) {
+            liveData.setValue(new ObservableArrayList() {
+                {
+                    if (liveData.getValue() != null) {
+                        addAll((Collection) liveData.getValue());
+                    }
+                }
+            });
+        }
+        return liveData;
     }
 }
